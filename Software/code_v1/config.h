073db0e382da1f787b90ce4eb600d2477b845364
messed-up-tables <77458654+messed-up-tables@users.noreply.github.com>
2024-07-02 18:08:25 +0600

    code

---
+++ Software/code_v1/config.h
@@ -0,0 +1,30 @@
+
+//-------------------------------------------------- P I N     M A P ----------------------------
+//-----------------------------------------------------------------------------------------------
+
+//--------------------------MOTOR PINS
+#define motor01_pwm 11
+#define motor01_L 40
+#define motor01_R 42
+
+#define motor02_pwm 10
+#define motor02_L 44
+#define motor02_R 46
+
+#define motor03_pwm 9
+#define motor03_L 36
+#define motor03_R 38
+
+#define motor04_pwm 8
+#define motor04_L 32
+#define motor04_R 34
+
+//----------------------------COMMUNICATION PINS
+#define telemetry_tx 14   \\Serial 01
+#define telemetry_rx 15
+
+#define radio_tx 16       \\ Serial 02
+#define radio_rx 17
+
+#define gps_tx 18         \\ Serial 03
+#define gps_rx 19
